2475fd6cb300030492d341a8ae64de31f1ca8022
Facundo Grasso <facu@miracledevs.com>
2017-02-14 15:49:42 -0300

    alien-shopping last changes

--- aliens-shopping.php
+++ aliens-shopping.php
@@ -3,6 +3,7 @@
 		<meta charset="utf-8">
 		<title>Aliens Shopping</title>
 		<link rel="stylesheet" href="style.css" type="text/css" >
+		<link rel="stylesheet" href="alienshoppingstyle.css" type="text/css" >
 		<script rel="C:\Users\facun\Repositorios\Universos-a-la-vuelta\Script" src="myScript.js"></script>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,6 +26,8 @@
 				<p class="initialtext"> Take a look and come to know us anytime!</p>
 			</div>
 		</div>
+
+		<!-- Alien Food Truck -->
 		<div class="alienfoodbanner">
 			<div class="wrapper-outterfoodtext">
 				<div class="container">
@@ -37,6 +40,8 @@
 				<p class="textafterfoodbanner">Yummy! The best food you can imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in iaculis nibh, et viverra justo. Aenean mauris nisl, vestibulum eget neque id, tincidunt elementum eros. Nulla iaculis vestibulum enim ac pretium. Proin at ex ante. Suspendisse eget fermentum neque, sed vestibulum dolor. Sed sed condimentum dui. In elementum, arcu sit amet vehicula mollis, purus dolor elementum est, maximus semper nisi est sit amet justo. Nullam non tincidunt sem, ut porttitor sapien. Aenean tincidunt, velit vel eleifend tincidunt, leo ligula iaculis enim, blandit lacinia velit turpis eu neque. Proin elementum, nulla vitae dignissim sagittis, lectus massa semper elit, et ultricies risus mauris id sapien. Sed porttitor sapien vel aliquam feugiat.</p>
 			</div>
 		</div>
+
+		<!-- Hospitalien -->
 		<div class="alienhospitalbanner">
 			<div class="wrapper-outterhospitaltext">
 				<div class="container">
@@ -49,28 +54,32 @@
 				<p class="textafterhospitalbanner">Yummy! The best food you can imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in iaculis nibh, et viverra justo. Aenean mauris nisl, vestibulum eget neque id, tincidunt elementum eros. Nulla iaculis vestibulum enim ac pretium. Proin at ex ante. Suspendisse eget fermentum neque, sed vestibulum dolor. Sed sed condimentum dui. In elementum, arcu sit amet vehicula mollis, purus dolor elementum est, maximus semper nisi est sit amet justo. Nullam non tincidunt sem, ut porttitor sapien. Aenean tincidunt, velit vel eleifend tincidunt, leo ligula iaculis enim, blandit lacinia velit turpis eu neque. Proin elementum, nulla vitae dignissim sagittis, lectus massa semper elit, et ultricies risus mauris id sapien. Sed porttitor sapien vel aliquam feugiat.</p>
 			</div>
 		</div>
+
+		<!-- Space Ship -->
 		<div class="alienshipbanner">
-			<div class="wrapper-outterfoodtext">
+			<div class="wrapper-alienshiptext">
 				<div class="container">
-					<h2 class="outterfoodtext">Food space truck</h2>
+					<h2 class="alienshiptext">Luxurious spaceships</h2>
 				</div>
 			</div>
 		</div>
 		<div class="container">
 			<div class="initialsection">
-				<p class="textafterfoodbanner">Yummy! The best food you can imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in iaculis nibh, et viverra justo. Aenean mauris nisl, vestibulum eget neque id, tincidunt elementum eros. Nulla iaculis vestibulum enim ac pretium. Proin at ex ante. Suspendisse eget fermentum neque, sed vestibulum dolor. Sed sed condimentum dui. In elementum, arcu sit amet vehicula mollis, purus dolor elementum est, maximus semper nisi est sit amet justo. Nullam non tincidunt sem, ut porttitor sapien. Aenean tincidunt, velit vel eleifend tincidunt, leo ligula iaculis enim, blandit lacinia velit turpis eu neque. Proin elementum, nulla vitae dignissim sagittis, lectus massa semper elit, et ultricies risus mauris id sapien. Sed porttitor sapien vel aliquam feugiat.</p>
+				<p class="textafterspacebanner">Yummy! The best food you can imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in iaculis nibh, et viverra justo. Aenean mauris nisl, vestibulum eget neque id, tincidunt elementum eros. Nulla iaculis vestibulum enim ac pretium. Proin at ex ante. Suspendisse eget fermentum neque, sed vestibulum dolor. Sed sed condimentum dui. In elementum, arcu sit amet vehicula mollis, purus dolor elementum est, maximus semper nisi est sit amet justo. Nullam non tincidunt sem, ut porttitor sapien. Aenean tincidunt, velit vel eleifend tincidunt, leo ligula iaculis enim, blandit lacinia velit turpis eu neque. Proin elementum, nulla vitae dignissim sagittis, lectus massa semper elit, et ultricies risus mauris id sapien. Sed porttitor sapien vel aliquam feugiat.</p>
 			</div>
 		</div>
+
+		<!-- Outter Sports -->
 		<div class="aliensportbanner">
-			<div class="wrapper-outterhospitaltext">
+			<div class="wrapper-aliensporttext">
 				<div class="container">
-					<h2 class="outterhospitaltext">Hospitalien</h2>
+					<h2 class="aliensporttext">Deep galaxy sports</h2>
 				</div>
 			</div>
 		</div>
 		<div class="container">
 			<div class="initialsection">
-				<p class="textafterhospitalbanner">Yummy! The best food you can imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in iaculis nibh, et viverra justo. Aenean mauris nisl, vestibulum eget neque id, tincidunt elementum eros. Nulla iaculis vestibulum enim ac pretium. Proin at ex ante. Suspendisse eget fermentum neque, sed vestibulum dolor. Sed sed condimentum dui. In elementum, arcu sit amet vehicula mollis, purus dolor elementum est, maximus semper nisi est sit amet justo. Nullam non tincidunt sem, ut porttitor sapien. Aenean tincidunt, velit vel eleifend tincidunt, leo ligula iaculis enim, blandit lacinia velit turpis eu neque. Proin elementum, nulla vitae dignissim sagittis, lectus massa semper elit, et ultricies risus mauris id sapien. Sed porttitor sapien vel aliquam feugiat.</p>
+				<p class="textaftersportbanner">Yummy! The best food you can imagine. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in iaculis nibh, et viverra justo. Aenean mauris nisl, vestibulum eget neque id, tincidunt elementum eros. Nulla iaculis vestibulum enim ac pretium. Proin at ex ante. Suspendisse eget fermentum neque, sed vestibulum dolor. Sed sed condimentum dui. In elementum, arcu sit amet vehicula mollis, purus dolor elementum est, maximus semper nisi est sit amet justo. Nullam non tincidunt sem, ut porttitor sapien. Aenean tincidunt, velit vel eleifend tincidunt, leo ligula iaculis enim, blandit lacinia velit turpis eu neque. Proin elementum, nulla vitae dignissim sagittis, lectus massa semper elit, et ultricies risus mauris id sapien. Sed porttitor sapien vel aliquam feugiat.</p>
 			</div>
 		</div>
 		
